681403d461ced2e8f6a9fe1a14f1bfb239e0779d
Buruiana Claudiu <claudiu.buruiana@vertistudio.com>
2016-08-25 09:52:41 +0300

    !!! #229 replace $wp_customize with is_customize_preview

--- inc/structure/header.php
+++ inc/structure/header.php
@@ -13,8 +13,6 @@ if ( ! function_exists( 'shop_isle_primary_navigation' ) ) {
 	 */
 	function shop_isle_primary_navigation() {
 
-		global $wp_customize;
-
 		?>
 		<!-- Navigation start -->
 		<nav class="navbar navbar-custom navbar-transparent navbar-fixed-top" role="navigation">
@@ -29,12 +27,12 @@ if ( ! function_exists( 'shop_isle_primary_navigation' ) ) {
 							echo '<div class="shop_isle_header_title"><div class="shop-isle-header-title-inner">';
 							if( !empty($shop_isle_logo) ):
 								echo '<a href="'.esc_url( home_url( '/' ) ).'" class="logo-image"><img src="'.esc_url( $shop_isle_logo ).'"></a>';
-								if( isset( $wp_customize ) ):
+								if( is_customize_preview() ):
 									echo '<h1 class="site-title shop_isle_hidden_if_not_customizer""><a href="'.esc_url( home_url( '/' ) ).'" title="'.esc_attr( get_bloginfo( 'name', 'display' ) ).'" rel="home">'.get_bloginfo( 'name' ).'</a></h1>';
 									echo '<h2 class="site-description shop_isle_hidden_if_not_customizer"><a href="'.esc_url( home_url( '/' ) ).'" title="'.esc_attr( get_bloginfo( 'name', 'display' ) ).'" rel="home">'.get_bloginfo( 'description' ).'</a></h2>';
 								endif;
 							else:
-								if( isset( $wp_customize ) ):
+								if( is_customize_preview() ):
 									echo '
 											<a href="'.esc_url( home_url( '/' ) ).'" class="logo-image shop_isle_hidden_if_not_customizer">
 												<img src="">
